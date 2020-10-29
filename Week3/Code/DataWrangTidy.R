00ca2fcbb53350c7f16bcd72c854eff0e83ff8cb
Yewshen Lim <y.lim20@imperial.ac.uk>
2020-10-29 19:41:54 +0000

    Recovered TAutoCorr.R, finished DataWrang/Tidy.R

---
+++ Week3/Code/DataWrangTidy.R
@@ -0,0 +1,55 @@
+################################################################
+################## Wrangling the Pound Hill Dataset ############
+################################################################
+
+# Author: Yewshen Lim 'y.lim20@imperial.ac.uk'
+# version: 0.0.1
+
+require(dplyr)
+require(tidyr)
+
+############# Load the dataset ###############
+# header = false because the raw data don't have real headers
+MyData <- as.matrix(read.csv("../data/PoundHillData.csv", header = FALSE))
+
+# header = true because we do have metadata headers
+MyMetaData <- read.csv("../data/PoundHillMetaData.csv", header = TRUE, sep = ";")
+
+############# Inspect the dataset ###############
+head(MyData)
+dim(MyData)
+dplyr::glimpse(MyData) # tidier str()
+
+############# Transpose ###############
+# To get those species into columns and treatments into rows 
+MyData <- t(MyData)
+head(MyData)
+dim(MyData)
+
+############# Replace species absences with zeros ###############
+MyData[MyData == ""] = 0
+
+############# Convert raw matrix to data frame ###############
+
+TempData <- as.data.frame(MyData[-1, ], stringsAsFactors = F) 
+#stringsAsFactors = F is important!
+colnames(TempData) <- MyData[1, ] # assign column names from original data
+
+############# Convert from wide to long format  ###############
+
+# using dplyr
+MyWrangledData <- TempData %>% gather(Species, Count, -Cultivation, -Block,
+    -Plot, -Quadrat)
+
+# set Cultivation, Block, Plot, Quadrat columns as factors and count as numeric
+MyWrangledData <- MyWrangledData %>% mutate(Cultivation = factor(Cultivation),
+    Block = factor(Block),
+    Plot = factor(Plot),
+    Quadrat = factor(Quadrat),
+    Count = as.integer(Count))
+
+dplyr::glimpse(MyWrangledData)
+head(MyWrangledData)
+dim(MyWrangledData)
+
+############# Exploring the data (extend the script below)  ###############
---
+++ Week3/Code/TAutoCorr.R
@@ -0,0 +1,75 @@
+# Author: Yewshen Lim (y.lim20@imperial.ac.uk)
+# Script: TAutoCorr.R
+# Created: Oct 2020
+#
+# This script calculates the correlation between n - 1 pairs of years, where n
+# is the total number of years
+
+# clear out workspace
+rm(list = ls())
+
+# set seed so results are reproducible
+set.seed(1)
+
+# load data
+load("../Data/KeyWestAnnualMeanTemperature.RData")
+
+# list the structure
+ls.str(ats)
+
+# plot
+tiff("../Results/TACplot1.tif", compression = "lzw")
+plot(ats$Year, ats$Temp,
+    xlab = "Year",
+    ylab = "Temp (Degrees celcius)",
+    type = "p")
+dev.off()
+
+# overall correlation
+# compute the correlation between successive years T[y] against T[y+1]
+# temp in y+1 is dependent on temp in y
+T_y0 <- c(ats[1:99, 2])
+T_y1 <- c(ats[2:100, 2])
+cc0 <- cor(T_y0, T_y1)
+cat("The correlation between successive years is", cc0)
+
+# generating 10000 samples of random ordered temperatures
+samples <- replicate(10000, sample(ats$Temp, replace = F))
+
+# generate a vector of corr coeffs from the 10000 random samples
+ccS <- vector("numeric", 10000)
+for (i in 1:10000) {
+    T_s <- cor(samples[1:99, i], samples[2:100, i])
+    ccS[i] <- T_s
+}
+
+# histogram to compare coefficient correlations
+tiff("../Results/TACplot2.tif", compression = "lzw")
+hist(ccS, xlim = c(-0.4, 0.4),
+    xlab = "Correlation coefficients of random sample",
+    main = NULL)
+abline(v = cc0, col = "red", lwd = 1, lty = 2)
+text(cc0 - 0.1, 1875, "original correlation coefficient \nfor successive years")
+dev.off()
+
+# calculating the estimated p-value
+p_estimate <- sum(ccS > cc0) / 10000
+cat("The estimated p-value is", p_estimate)
+
+# This gives a different value because temp is correlated to year
+# cc1 <- cor(ats$Year, ats$Temp, method = "pearson")
+# cc1
+
+# sample_year <- function(vector) {
+#     randYear <- sample(vector, length(vector), replace = FALSE)
+#     return(cor(randYear, ats$Temp))
+# }
+
+# sapply_sample <- function(vector, num) {
+#     results <- sapply(1:num, function(i) sample_year(vector))
+#     # return(results)
+#     p_estimate = (sum(results > cc0)) / num
+#     return(p_estimate)
+# }
+
+# sapply_sample(ats$Year, 10000)
